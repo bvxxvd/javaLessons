3b8f97b742f91d6011ef2bde956628f1bbb4a531
bvxxvd <bxvvxd@gmail.com>
2022-04-04 21:43:12 +0300

    Ветка 15

---
+++ src/test/java/Lesson15/BaseTest.java
@@ -0,0 +1,36 @@
+package Lesson15;
+
+import Lesson15.pages.MainPage;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.uncommons.reportng.HTMLReporter;
+
+import java.util.concurrent.TimeUnit;
+
+@Listeners({HTMLReporter.class})
+public class BaseTest {
+
+    private static final Logger LOG = Logger.getLogger(BaseTest.class);
+    protected WebDriver driver;
+
+    @BeforeClass
+    public WebDriver startBrowser() {
+        LOG.warn("start, 'startBrowser'");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+        LOG.warn("finish, 'startBrowser'");
+        return driver;
+    }
+
+    protected MainPage navigate(String url) {
+        LOG.info("start, 'navigate;");
+        driver.get(url);
+        LOG.info("finish, 'navigate;");
+        return new MainPage(driver);
+    }
+}
