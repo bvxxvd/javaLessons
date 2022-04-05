645503cd50ce1079c9ec4d1b89114876d9e1dbf9
bvxxvd <bxvvxd@gmail.com>
2022-04-05 12:15:43 +0300

    Изменение инициализации вебдрайвера

--- src/test/java/Lesson15/BaseTest.java
+++ src/test/java/Lesson15/BaseTest.java
@@ -1,36 +1,34 @@
 package Lesson15;
 
 import Lesson15.pages.MainPage;
-import org.apache.log4j.Logger;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Listeners;
-import org.uncommons.reportng.HTMLReporter;
 
 import java.util.concurrent.TimeUnit;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-@Listeners({HTMLReporter.class})
 public class BaseTest {
 
-    private static final Logger LOG = Logger.getLogger(BaseTest.class);
+
     protected WebDriver driver;
 
     @BeforeClass
     public WebDriver startBrowser() {
-        LOG.warn("start, 'startBrowser'");
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-        LOG.warn("finish, 'startBrowser'");
+
         return driver;
     }
 
     protected MainPage navigate(String url) {
-        LOG.info("start, 'navigate;");
+
         driver.get(url);
-        LOG.info("finish, 'navigate;");
+
         return new MainPage(driver);
     }
 }
--- src/test/java/Lesson15/LoginTest.java
+++ src/test/java/Lesson15/LoginTest.java
@@ -2,7 +2,6 @@ package Lesson15;
 
 import static org.testng.Assert.assertTrue;
 
-import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
 import Lesson15.pages.MailBoxPage;
@@ -10,25 +9,18 @@ import Lesson15.pages.MainPage;
 
 public class LoginTest extends BaseTest {
 
-    private static final Logger LOG = Logger.getLogger(LoginTest.class);
 
     @Test
     public void doLogin() {
 
-        LOG.warn("start: navigate");
         MainPage mainPage = navigate(MainPage.URL);
 
-        LOG.warn("start: login");
-        LOG.info("enter: daniel_defo@internet.ru  iiYiYyoPM31;");
         MailBoxPage mailBox = mainPage.login("daniel_defo@internet.ru", "iiYiYyoPM31;");
 
-        LOG.info("send Email;");
         mailBox.sendEmail("daniel_defo@internet.ru", "Interesting topic", "EMAIL_TEXT");
 
-        LOG.info("check Email;");
         String titleEmail = mailBox.waitForEmail("Interesting topic");
         assertTrue(titleEmail.contains("Interesting topic"));
 
-        LOG.info("check OK;");
     }
 }
