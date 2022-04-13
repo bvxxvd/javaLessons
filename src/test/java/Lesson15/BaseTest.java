81a4da21992125e76b7be5564aa392e666c9f61a
bvxxvd <bxvvxd@gmail.com>
2022-04-13 11:52:39 +0300

    1

---
+++ src/test/java/Lesson15/BaseTest.java
@@ -0,0 +1,33 @@
+package Lesson15;
+
+import Lesson15.pages.MainPage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeClass
+    public WebDriver startBrowser() {
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+
+        return driver;
+    }
+
+
+    protected MainPage navigate(String url) {
+        driver.get(url);
+        return new MainPage(driver);
+    }
+}
---
+++ src/test/java/Lesson15/LoginTest.java
@@ -0,0 +1,22 @@
+package Lesson15;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+import Lesson15.pages.MailBoxPage;
+import Lesson15.pages.MainPage;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void doLogin() {
+
+        MainPage mainPage = navigate(MainPage.URL);
+        MailBoxPage mailBox = mainPage.login("daniel_defo@internet.ru", "iiYiYyoPM31;");
+        mailBox.sendEmail("daniel_defo@internet.ru", "Interesting topic", "EMAIL_TEXT");
+
+        String titleEmail = mailBox.waitForEmail("Interesting topic");
+        assertTrue(titleEmail.contains("Interesting topic"));
+    }
+}
